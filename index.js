abe28dd49ee475c08c468cb56c20f1d9b8cbb757
becakdarurat <becakdarurat>
2024-10-22 21:40:39 +0700

    "#2 Hitung angka paling besar"

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ function countVowels ( str ) {
     for (const char of str) {
         if (vowels.includes(char)) {
             count++;
-        } else {
-            
         }
     }
     return count;
@@ -21,3 +19,27 @@ function countVowels ( str ) {
 //  Menampilkan hasil
 const inputStr1 = `Hello Worl!`;
 console.log(`Jumlah vokal dalam "${inputStr1}": ${countVowels(inputStr1)}`);
+
+/* 
+    Soal 2: Nilai Tertinggi
+
+    Buatlah fungsi findMax yang menerima
+    array angka dan mengembalikan nilai
+    tertinggi dalam array tersebut.
+*/
+
+function findMax(arr) {
+    if ( arr.length === 0 ) return null; // Menghindari jika arraynya kosong
+    let max = arr[0];
+
+    for (const item of arr) {
+        if ( item > max ) { 
+            max = item;
+        }
+    }
+    return max;
+}
+
+// Menampilkan hasil
+const numbers = [1,5,3,9,2];
+console.log(`Nilai tertinggi dari [${numbers}]: ${findMax(numbers)}`);
